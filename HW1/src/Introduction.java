39f3f8fb60638cb03ee4106b00d4c017a84a3c52
tranvietoan <tranviettoan2712@gmail.com>
2025-09-21 19:22:36 +0700

    nộp HW1

---
+++ HW1/src/Introduction.java
@@ -0,0 +1,13 @@
+
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Trần Việt Toàn\t24021642\tCS-5\ttranvietoan\ttranvietoan2712@gmail.com");
+
+        for(int i=9;i>=1;i--){
+            System.out.println(i+" bottles of beer on the wall, "+i+" 9 bottles of beer.");
+            System.out.println("Take one down, pass it around, ");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+
+    }
+}
